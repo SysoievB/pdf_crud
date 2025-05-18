f5d743051cdb66976401bed59ccc6a99269596dd
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-05-18 08:11:15 +0200

    Init commit

---
+++ src/main/java/com/pdf_crud/PdfCrudApplication.java
@@ -0,0 +1,13 @@
+package com.pdf_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdfCrudApplication.class, args);
+    }
+
+}
